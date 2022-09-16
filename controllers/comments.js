9443a84b623eb69db23dd24faf53ee25a3b12be3
devin-lynch <devin.l.lynch@gmail.com>
2022-09-16 14:14:49 -0500

    Stub post route for comment

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,7 +3,22 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models')
 
-
+router.post('/', async (req, res) => {
+    try {
+        console.log('RIGHT HERE!!!!!!!!!', req.body)
+        //findOrCreate the comment in the db
+        const [comment] = await db.comment.findOrCreate({
+            where: {
+                description: req.body.description,
+                userId: res.locals.user,
+                playerId: req.body.playerId
+            }
+        })
+    } catch(err) {
+        console.warn(err)
+        res.send(`Server Error!`)
+    }
+})
 
 
 
